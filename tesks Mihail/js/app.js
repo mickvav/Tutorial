9c80876d285ec87c3192c88fc870bcdcd6fb22ca
Anton <104930896+Zmiievskyi@users.noreply.github.com>
2022-08-07 13:03:57 +0100

    getmax4

--- tesks Mihail/js/app.js
+++ tesks Mihail/js/app.js
@@ -11,3 +11,17 @@ return newArray;
 
 
  console.log(getmax3([5, 1, 8, 10, 4, 90, 1, 92, 93, 3]));
+
+
+
+ 
+ function getmax4(arrey) {
+
+   for (let i = 0; i < 4; i += 1) {
+     let biggestNumber = Math.max(...arrey);
+     arrey.pop(biggestNumber);
+   }
+   return arrey;
+ }
+
+console.log(getmax4([1, 2, 3, 90, 91, 92]));
