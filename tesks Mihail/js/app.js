fab9d40eae79fdc34585e1cacd4b808c0d64cf53
Anton <104930896+Zmiievskyi@users.noreply.github.com>
2022-08-09 19:16:51 +0100

    last version

--- tesks Mihail/js/app.js
+++ tesks Mihail/js/app.js
@@ -1,21 +1,35 @@
-
-
-function getmax4(arrey) {
+function getmax3(arrey) {
   const bigNumArrey = [];
+  const justBigNumArrey = [];
   let count = 3;
-  for (let i = 0; i < count; i += 1) {
-    let biggestNumber = Math.max(...arrey);
-    let position = arrey.indexOf(biggestNumber);
 
-    if(bigNumArrey.includes(biggestNumber)) {
-      count+=1
+  getBigNumArrey();
+  function getBigNumArrey() {
+    for (let i = 0; i < count; i += 1) {
+      let biggestNumber = Math.max(...arrey);
+      let position = arrey.indexOf(biggestNumber);
+      if (bigNumArrey.includes(biggestNumber)) {
+        count += 1;
+      }
+
+      bigNumArrey.push(biggestNumber);
+      arrey.splice(position, 1);
     }
-
-    bigNumArrey.push(biggestNumber);
-    arrey.splice(position, 1);
+    return bigNumArrey;
   }
-  console.log(bigNumArrey);
-  return bigNumArrey;
-}
 
-console.log(getmax4([1, 2, 3, 4, 5, 5, 4, 3, 2, 1]));
+  justBigest();
+  function justBigest() {
+    for (let i = 0; i < bigNumArrey.length; i += 1) {
+      // let position2 = bigNumArrey.indexOf(i);
+      if (bigNumArrey.includes(bigNumArrey[i])) {
+        justBigNumArrey.push(bigNumArrey[i]);
+      }
+      bigNumArrey.splice(justBigNumArrey.indexOf(bigNumArrey[i]), 1);
+    }
+    return justBigNumArrey;
+  };
+
+  return justBigNumArrey;
+}
+console.log(getmax3([1, 2, 3, 4, 5, 5, 4, 3, 2, 1]));
