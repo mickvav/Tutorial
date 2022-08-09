578eab17530380c60b90d169853383d292a2c768
Anton <104930896+Zmiievskyi@users.noreply.github.com>
2022-08-09 17:27:52 +0100

    the biggest number

--- tesks Mihail/js/app.js
+++ tesks Mihail/js/app.js
@@ -1,29 +1,21 @@
 
-function getmax3(arrey) {
-  let newArray = arrey;
 
-  for (let i =0; i<3; i+=1) {
-  let biggestNumber = Math.max(...newArray);
-  newArray.splice(newArray.indexOf(biggestNumber),1);
-  
+function getmax4(arrey) {
+  const bigNumArrey = [];
+  let count = 3;
+  for (let i = 0; i < count; i += 1) {
+    let biggestNumber = Math.max(...arrey);
+    let position = arrey.indexOf(biggestNumber);
+
+    if(bigNumArrey.includes(biggestNumber)) {
+      count+=1
+    }
+
+    bigNumArrey.push(biggestNumber);
+    arrey.splice(position, 1);
+  }
+  console.log(bigNumArrey);
+  return bigNumArrey;
 }
-return newArray;
-}
 
-
- console.log(getmax3([1, 2, 3, 4, 5,    5, 4, 3, 2, 1]));
-
-
-
-
- function getmax4(arrey) {
-
-   for (let i = 0; i < 4; i += 1) {
-     let biggestNumber = Math.max(...arrey);
-     let position = arrey.indexOf(biggestNumber);
-     arrey.splice(position, 1);
-   }
-   return arrey;
- }
-
-console.log(getmax4([1, 2, 3, 4, 5,      5, 4, 3, 2, 1]));
+console.log(getmax4([1, 2, 3, 4, 5, 5, 4, 3, 2, 1]));
